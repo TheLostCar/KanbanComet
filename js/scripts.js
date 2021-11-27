1cf7edffe60471b1beb5f8c9162250f556e1084f
Carlos Aranzana <carlos.aranzana2001@gmail.com>
2021-11-27 01:25:56 -0500

    hide password text, report validity of getStarted email

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,23 @@ $(function () {
 
     let getStartedInput = $('#getStartedEmailInput')
     let getStartedForm = $('#getStartedForm')
-    console.log(getStartedForm)
+    let getStartedButton = $('#getStartedButton')
 
     getStartedInput.on('keyup', function () {
         // updates email value with every key press
         getStartedForm.attr('action', `/signup.html?email=${this.value}`);
 
+    })
+
+
+    getStartedButton.on('click', function () {
+        //check to see if email is valid
+        if (getStartedInput[0].matches('input:valid')) {
+            window.location.href = getStartedForm.attr('action')
+        } else {
+            //report invalid email to user
+            getStartedForm[0].reportValidity();
+        }
     })
 
     // applies email from index.html to signup form
